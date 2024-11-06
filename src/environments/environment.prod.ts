a5aec0d3e9c3793eefe7682fcb77f27114e51944
venkateshdev22 <pvenkateshnaidu@gmail.com>
2024-11-06 19:12:52 +0530

    Initial Files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.yourproductiondomain.com',  // Production backend API endpoint
+  featureFlag: false,  // Example of a feature toggle for production
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.ts
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',  // Local backend API endpoint
+  featureFlag: true,  // Example of a feature toggle for local
+};
